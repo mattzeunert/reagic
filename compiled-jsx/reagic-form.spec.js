dae56be950ba9e3880738671028957ef2c47b8e0
Matt Zeunert <matt@mostlystatic.com>
2015-09-13 12:20:41 +0100

    Add test for ReagicForm onChange.

--- compiled-jsx/reagic-form.spec.js
+++ compiled-jsx/reagic-form.spec.js
@@ -2,16 +2,34 @@ var ReagicForm = require("./reagic-form.js");
 var React = require('react/addons');
 
 describe("ReagicForm Component", function(){
-    it("Renders something", function(){
-        var data = {
+    var data = {
           name: "Tom"
-        };
+        },
+        domNode,
+        onChange;
 
-        var domNode = React.addons.TestUtils.renderIntoDocument(
-            React.createElement(ReagicForm, {data: data})
+    beforeEach(function(){
+        domNode = React.addons.TestUtils.renderIntoDocument(
+            React.createElement(ReagicForm, {data: data, onChange: function(){
+                onChange.apply(this, arguments);
+            }})
         ).getDOMNode();
+    })
 
+    it("Renders an input", function(){
         expect(domNode.innerHTML).toContain("<input");
-
     });
+
+    it("Triggers onChange with the new data if the value is edited", function(done){
+        onChange = function(newData){
+            expect(newData).toEqual({name: "Thomas"})
+            done();
+        }
+
+        var input = domNode.querySelector("input");
+        input.value = "Thomas";
+        React.addons.TestUtils.Simulate.change(input);
+    })
 });
+
+
