96fc27ad87cefd8670cf690d12370a89c2e9006b
Matt Zeunert <matt@mostlystatic.com>
2015-09-17 15:55:10 +0100

    Keep component reference in reagin-form spec.

--- app/reagic-form.spec.js
+++ app/reagic-form.spec.js
@@ -1,20 +1,21 @@
 var ReagicForm = require("./reagic-form.js");
 var React = require('react/addons');
-var renderToDOMNode = require("./render-to-dom-node.js");
 
 describe("ReagicForm Component", function(){
     var data = {
           name: "Tom"
         },
         domNode,
+        component,
         onChange;
 
     beforeEach(function(){
-        domNode = renderToDOMNode(
+        component = React.addons.TestUtils.renderIntoDocument(
             <ReagicForm data={data} onChange={function(){
                 onChange.apply(this, arguments);
             }} />
         );
+        domNode = React.findDOMNode(component);
     })
 
     it("Renders an input", function(){
